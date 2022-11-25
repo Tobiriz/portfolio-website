5c55745ab5ecfba844362b61ad31bc010483db30
Tobias <tobias.weinlich@gmx.de>
2022-11-25 09:05:22 +0100

    Addes shadow

--- img.js
+++ img.js
@@ -1,22 +1,13 @@
 function toggleProjectsBox() {
     const artBox = document.getElementById("article-projects-box-id");
     const infoBox = document.getElementById("projects-info-box-id");
-
+    const shadow = document.getElementById("shadow");
     
     if(document.body.dataset.projects == "false") {
-        adjustZAxis(1);
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--gradient-color-white-0),"
-        + "var(--gradient-color-white-1)), url('img/projects_crop.jpg')";
-        artBox.style.scale = "1.1";
-        infoBox.style.opacity = "1.0";
+        showArticle(artBox, infoBox, shadow, "-25%", "projects");
         document.body.dataset.projects = "true";
     } else {
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--background-color-transparent),"
-        + "var(--background-color)), url('img/projects_crop.jpg')";
-        artBox.style.scale = "1";
-        infoBox.style.opacity = "0";
+        hideArticle(artBox, infoBox, shadow, "projects");
         document.body.dataset.projects = "false";
     }
 }
@@ -24,22 +15,13 @@ function toggleProjectsBox() {
 function toggleProfileBox() {
     const artBox = document.getElementById("article-profile-box-id");
     const infoBox = document.getElementById("profile-info-box-id");
-
+    const shadow = document.getElementById("shadow");
     
     if(document.body.dataset.profile == "false") {
-        adjustZAxis(2);
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--gradient-color-white-0),"
-        + "var(--gradient-color-white-1)), url('img/profile_crop.jpg')";
-        artBox.style.scale = "1.1";
-        infoBox.style.opacity = "1.0";
+        showArticle(artBox, infoBox, shadow, "75%", "profile");
         document.body.dataset.profile = "true";
     } else {
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--background-color-transparent),"
-        + "var(--background-color)), url('img/profile_crop.jpg')";
-        artBox.style.scale = "1";
-        infoBox.style.opacity = "0";
+        hideArticle(artBox, infoBox, shadow, "profile");
         document.body.dataset.profile = "false";
     }
 }
@@ -47,44 +29,32 @@ function toggleProfileBox() {
 function toggleContactBox() {
     const artBox = document.getElementById("article-contact-box-id");
     const infoBox = document.getElementById("contact-info-box-id");
-
+    const shadow = document.getElementById("shadow");
     
     if(document.body.dataset.contact == "false") {
-        adjustZAxis(3);
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--gradient-color-white-0),"
-        + "var(--gradient-color-white-1)), url('img/contact_crop.jpg')";
-        artBox.style.scale = "1.1";
-        infoBox.style.opacity = "1.0";
+        showArticle(artBox, infoBox, shadow, "175%", "contact");
         document.body.dataset.contact = "true";
     } else {
-        artBox.style.backgroundImage =
-        "linear-gradient(to bottom, var(--background-color-transparent),"
-        + "var(--background-color)), url('img/contact_crop.jpg')";
-        artBox.style.scale = "1";
-        infoBox.style.opacity = "0";
+        hideArticle(artBox, infoBox, shadow, "contact");
         document.body.dataset.contact = "false";
     }
 }
 
-function adjustZAxis(article) {
-    const projects = document.getElementById("article-projects-box-id");
-    const profile = document.getElementById("article-profile-box-id");
-    const contact = document.getElementById("article-contact-box-id");
+function showArticle(artBox, infoBox, shadow, left, url) {
+    artBox.style.backgroundImage =
+        "linear-gradient(to bottom, var(--gradient-color-white-0),"
+        + "var(--background-color-02)), url('img/" + url + "_crop.jpg')";
+    artBox.style.scale = "0.9";
+    infoBox.style.opacity = "1.0";
+    shadow.style.left = left;
+    shadow.style.background = "radial-gradient(var(--gradient-color-white-1) 40%, var(--gradient-color-white-0) 60%)";
+}
 
-    if(article == 1) {
-        projects.style.zIndex = "2";
-        profile.style.zIndex = "1";
-        contact.style.zIndex = "1";
-    } 
-    else if(article == 2) {
-        projects.style.zIndex = "1";
-        profile.style.zIndex = "2";
-        contact.style.zIndex = "1";
-    }
-    else {
-        projects.style.zIndex = "1";
-        profile.style.zIndex = "1";
-        contact.style.zIndex = "2";
-    }
+function hideArticle(artBox, infoBox, shadow, url) {
+    artBox.style.backgroundImage =
+        "linear-gradient(to bottom, var(--background-color-transparent),"
+        + "var(--background-color)), url('img/" + url + "_crop.jpg')";
+        artBox.style.scale = "0.8";
+        infoBox.style.opacity = "0";
+        shadow.style.background = "0";
 }
